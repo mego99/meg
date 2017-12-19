5a706bbc3fa98756d5481ea122ec5180aa74472b
Meg <megunaokawa@gmail.com>
2017-12-20 01:49:25 +0900

    added basic page scaffolding

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
+import ReactDOM from 'react-dom';
 import React, { Component } from "react";
 import "./index.css";
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
+import {Route,NavLink,HashRouter} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+// import Dropdown from "./Dropdown";
 
 class App extends Component {
   render() {
     return (
       <HashRouter>
         <div>
-          <header class="app-header">
-            <ul class ="nav-ul">
-              <li class="nav-li"><NavLink to="/">Home</NavLink></li>
-              <li class="nav-li"><NavLink to="/">Work</NavLink></li>
-              <li class="nav-li"><NavLink to="/about">Links</NavLink></li>
-              <li class="nav-li"><a href="https://playground.meguna.co" target="_blank" >Playground</a></li>
+          <header className="app-header">
+            <ul className="nav-ul">
+              <li className="nav-li"><NavLink to="/">Home</NavLink></li>
+              <li className="nav-li"><NavLink to="/">Work</NavLink></li>
+              <li className="nav-li"><NavLink to="/about">Links</NavLink></li>
+              <li className="nav-li"><a href="https://playground.meguna.co" target="_blank" >Playground</a></li>
             </ul>
-            <ul class="nav-ul">
-              <li class="nav-li" ><NavLink to="/">Art</NavLink></li>
-              <li class="nav-li" ><NavLink to="/">Code</NavLink></li>
-              <li class="nav-li" ><NavLink to="/">Design</NavLink></li>
+            <ul className="nav-ul" id="dropdown">
+              <li className="nav-li" ><NavLink to="/">Art</NavLink></li>
+              <li className="nav-li" ><NavLink to="/">Code</NavLink></li>
+              <li className="nav-li" ><NavLink to="/">Design</NavLink></li>
             </ul>
           </header>
-          <div class="content">
+          <div className="content">
             <Route exact path="/" component={Home}/>
             <Route exact path="/about" component={About}/>
           </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,9 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
+        <h1>Meguna Okawa</h1>
+        <p>Hi, I\'m Meguna! Check out some of my work here. </p>
 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
       </div>
     );
   }
