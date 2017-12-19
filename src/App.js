0385bad08fae434f860ff134dd3373fe2fc4d24d
Meg <megunaokawa@gmail.com>
2017-12-20 00:54:32 +0900

    updated navigation and added router links

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from "react";
+import "./index.css";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 class App extends Component {
   render() {
     return (
-        <header class="app-header">
-          <ul class ="nav-ul">
-            <li class="nav-li"><a href=''>Home</a></li>
-            <li class="nav-li"><a href=''>Work</a></li>
-            <li class="nav-li"><a href=''>Links</a></li>
-            <li class="nav-li"><a href="https://playground.meguna.co" target="_blank" >Playground</a></li>
-          </ul>
-          <ul class="nav-ul">
-            <li class="nav-li" ><a href=''>Art</a></li>
-            <li class="nav-li" ><a href=''>Code</a></li>
-            <li class="nav-li" ><a href=''>Design</a></li>
-          </ul>
-        </header>
+      <HashRouter>
+        <div>
+          <header class="app-header">
+            <ul class ="nav-ul">
+              <li class="nav-li"><NavLink to="/">Home</NavLink></li>
+              <li class="nav-li"><NavLink to="/">Work</NavLink></li>
+              <li class="nav-li"><NavLink to="/about">Links</NavLink></li>
+              <li class="nav-li"><a href="https://playground.meguna.co" target="_blank" >Playground</a></li>
+            </ul>
+            <ul class="nav-ul">
+              <li class="nav-li" ><NavLink to="/">Art</NavLink></li>
+              <li class="nav-li" ><NavLink to="/">Code</NavLink></li>
+              <li class="nav-li" ><NavLink to="/">Design</NavLink></li>
+            </ul>
+          </header>
+          <div class="content">
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About}/>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
