da72f743486451a20c97bee97aee90798312bbc2
Meg <megunaokawa@gmail.com>
2017-12-27 15:01:44 +0900

    link to post data

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {Route,NavLink,HashRouter} from "react-router-dom";
+import Post from './Post';
 
 class Work extends React.Component {
   constructor(props) {
@@ -38,22 +39,24 @@ class Work extends React.Component {
     let posts = this.state.posts;
     let date = this.adjustDate;
     let newarr =  Object.keys(posts).map(function(x,i) {
-          return <div key={i}>
-                  <h3>{date(posts[x].create_time)}</h3>
-                  <a href="" className="post-title">{posts[x].title}</a>
-                  <p className="post-content">{posts[x].content}</p>
-
-                </div>;
+          return  <div key={i}>
+                    <h3>{date(posts[x].create_time)}</h3>
+                    <NavLink to={"/posts/" + posts[x].id} className="post-title">{posts[x].title}</NavLink>
+                    <p className="post-content">{posts[x].content}</p>
+                    <Route exact path={"/posts/" + posts[x].id} component={Post} />
+                  </div>;
       })
     return newarr;
   }
 
   render() {
     return (
+      <HashRouter>
       <div>
         <h2>Work</h2>
           <div>{this.getPosts()}</div>
       </div>
+      </HashRouter>
     );
   }
 }
