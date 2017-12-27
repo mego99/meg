545fd3ca328185ffb06940dcb78b44afe9a5eea3
Meg <megunaokawa@gmail.com>
2017-12-27 15:02:12 +0900

    link to work page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./index.css";
 import {Route,NavLink,HashRouter} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-// import Dropdown from "./Dropdown";
+import Work from "./pages/Work";
 
 class App extends Component {
   render() {
@@ -14,7 +14,7 @@ class App extends Component {
           <header className="app-header">
             <ul className="nav-ul">
               <li className="nav-li"><NavLink to="/">Home</NavLink></li>
-              <li className="nav-li"><NavLink to="/">Work</NavLink></li>
+              <li className="nav-li"><NavLink to="/work">Work</NavLink></li>
               <li className="nav-li"><NavLink to="/about">Links</NavLink></li>
               <li className="nav-li"><a href="https://playground.meguna.co" target="_blank" >Playground</a></li>
             </ul>
@@ -27,6 +27,7 @@ class App extends Component {
           <div className="content">
             <Route exact path="/" component={Home}/>
             <Route exact path="/about" component={About}/>
+            <Route exact path="/work" component={Work}/>
           </div>
         </div>
       </HashRouter>
