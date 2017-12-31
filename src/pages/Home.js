3ecb7bd587f1a96a8414f9a63406b2130f7ee382
Meg <megunaokawa@gmail.com>
2018-01-01 07:08:06 +0900

    added crystals to non-index pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,12 @@ import './Home.css';
 class Home extends React.Component {
   componentDidMount() {
     document.body.classList.toggle("home-background",true);
+    document.body.classList.toggle("page-background",false);
   };
   componentWillUnmount() {
     document.body.classList.toggle("home-background",false);
+    document.body.classList.toggle("page-background",true);
+
   };
 
   render() {
