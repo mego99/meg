b42286fae3baa09133c9888e42360471a9cdd3f2
Meg <megunaokawa@gmail.com>
2018-01-03 16:32:32 +0900

    added syntax highlighting to post content and updated styles. some work for image import is started but all commented

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import {Switch,Route} from "react-router-dom";
 import marked from 'marked';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/vs2015.css';
+
+let te = require('text-encoding');
 
 class Post extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
+      posts: [],
+      post_images: []
     }
   }
 
@@ -20,31 +25,56 @@ class Post extends Component {
         this.setState({posts: parsedData});
         this.setState({post_title:parsedData[0]['title']});
         document.title = 'Meguna | ' + this.state.post_title;
-
       })
 
+    fetch(`/api/getpostimages/${this.props.match.params.id}`)
+      .then(
+        response => response.json()
+      )
+      .then(parsedData => {
+        console.log(parsedData);
+        this.setState({post_images: parsedData});
+      })
   }
 
   getPosts() {
     let getMarkdownText = this.getMarkdownText;
-    console.log(this.state.posts);
     let posts = this.state.posts;
     let newarr =  Object.keys(posts).map(function(x,i) {
-      let content = posts[x].content;
+      try {
+        return <div key={i}>
+                  <h1>{posts[x].title}</h1>
+                  <div dangerouslySetInnerHTML={getMarkdownText(posts[x].content)} />
+               </div>;
+      }
+      catch(e) {
+        return <div></div>
+      };
+    })
+    return newarr;
+  }
 
-      return <div key={i}>
-                <h1>{posts[x].title}</h1>
-                <div dangerouslySetInnerHTML={getMarkdownText(posts[x].content)} />
-             </div>;
-      })
+  getImages() {
+    let TextDecoder = te.TextDecoder;
+    let images = this.state.post_images;
+    console.log(images);
+    let newarr =  Object.keys(images).map(function(x,i) {
+      let baseStr = 'data:image/jpeg;base64,';
+      console.log(images[x]);
+      // let data = btoa(String.fromCharCode.apply(null,images[x].image.data));
+      // console.log(new TextDecoder('base-64').decode(new Uint8Array(images[x].image.data)));
+      console.log(images[x].image);
+      console.log(images[x].image.data);
+      return <div key={i} >
+              <img className="post-image" src={baseStr}></img>
+              <p>image</p>
+             </div>
+    })
     return newarr;
   }
 
   getMarkdownText(md) {
     let renderer = new marked.Renderer();
-
-// `${process.env.PUBLIC_URL}/header-images/${posts[x].image_link}-25.png`
-
     marked.setOptions({
       renderer: new marked.Renderer(),
       gfm: true,
@@ -53,7 +83,10 @@ class Post extends Component {
       pedantic: false,
       sanitize: false,
       smartLists: true,
-      smartypants: true
+      smartypants: true,
+      highlight: function (code) {
+        return require('highlight.js').highlightAuto(code).value;
+      }
     });
     let rawMarkup = marked(md, {sanitize: true});
     return { __html: rawMarkup };
