34a2dbb643e187a0bd1abe5bfe73136ec79212dc
Meg <megunaokawa@gmail.com>
2018-03-28 13:29:04 +0900

    fixed error in the call url for retrieving post image from api

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -81,7 +81,7 @@ class Post extends Component {
       let img = '';
       imageExists('/api/static/post-images/' + href) ? img = `${href}` : img = 'def.png'
       return `<img
-                class=post-image src=/api/static/post-images/${img} alt=${text} 
+                class=post-image src=/api/static/post-images/${img} alt=${text}
               />`;
     };
 
