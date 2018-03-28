20dfca29cc37d23073ceb8eeed3d9067f1458644
Meg <megunaokawa@gmail.com>
2018-03-28 13:29:37 +0900

    minor style and about page changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,9 +5,9 @@ class About extends React.Component {
     return (
       <div>
         <h2>About</h2>
-        <p>Hi! I'm Meguna, currently a high school senior in Tokyo. This is my personal blog.</p>
-        <p>I mostly post code- and art-related things.</p>
-        <p>For info on this website and how it was built, check <a href="">this post</a>. Alternatively, check out the <a href="https://github.com/mego99/meg">github repo</a>.</p>
+        <p>Hi! I'm Meguna, and this is my personal blog.</p>
+        <p>I'm currently a high school senior in Tokyo and will be attending Tufts University in the fall.</p>
+        <p>For info on this website and how it was built, check <a href="/tag/website-2.0">these posts</a>. Alternatively, check out the <a href="https://github.com/mego99/meg">github repo</a>.</p>
 
       </div>
     );
