19af74de1a28a1820c7cf0d5db87f884c0c00cc1
Meg <megunaokawa@gmail.com>
2019-02-05 17:01:20 -0500

    insert sample FeaturedPosts into index page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import CanvasShader from './CanvasShader.js';
+import FeaturedPost from './FeaturedPost.js';
 import './Home.css';
 
 class Home extends React.Component {
@@ -17,6 +18,14 @@ class Home extends React.Component {
     return (
       <div>
         <CanvasShader />
+        <FeaturedPost postId="11" 
+                      tagLine="featured work" 
+                      catLabel="view other work"
+                      catLink="/code"/>
+        <FeaturedPost postId="15" 
+                      tagLine="newest blog post" 
+                      catLabel="view blogs"
+                      catLink="/code"/>              
       </div>
     );
   }
