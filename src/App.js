ab2183461deea129cd23c73c35675911fdcb4db4
Meg <megunaokawa@gmail.com>
2019-08-27 01:48:18 +0900

    get rid of unused or broken routes

--- src/App.js
+++ src/App.js
@@ -1,65 +1,34 @@
-// import ReactDOM from 'react-dom';
-import React, { Component } from "react";
-import "./index.css";
-import {Route,NavLink,Switch} from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Work from "./pages/Work";
-import Art from './pages/Art';
-import Code from './pages/Code';
-import Design from './pages/Design';
-import PostRouter from './pages/PostRouter';
-import EditPost from './pages/EditPost';
-import NewPost from './pages/NewPost';
-import Playground from './pages/Playground';
-import PostList from './pages/PostList';
-import TagRouter from './pages/TagRouter';
-import ArtPostList from './pages/ArtPostList';
+import React, { PureComponent } from 'react';
+import './index.css';
+import { Route, NavLink, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Post from './pages/Post';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      postlistorigin: ''
-    };
-  }
+class App extends PureComponent {
   render() {
     return (
-        <div className="app-container">
-          <header className="app-header">
-            <NavLink exact to="/" activeClassName="title-active">
-              <h1 className="header-title">Meguna's Playground</h1>
-            </NavLink>
-            <ul className="nav-ul">
-              <li className="nav-li"><NavLink exact to="/">Home</NavLink></li>
-              <li className="nav-li"><NavLink to="/about">About</NavLink></li>
-            </ul>
-            <ul className="nav-ul" id="dropdown">
-              <li className="nav-li"><NavLink to="/art">Art</NavLink></li>
-              <li className="nav-li"><NavLink to="/code">Code</NavLink></li>
-            </ul>
-          </header>
-          <main>
-            <Switch>
-              <Route exact path="/about" component={About}/>
-              <Route exact path="/work" component={Work}/>
-              <Route exact path="/art" component={Art}/>
-              <Route exact path="/code" component={Code}/>
-              <Route exact path="/design" component={Design}/>
-              <Route exact path="/" component={Home}/>
-              <Route path="/allposts" component={PostRouter}/>
-              <Route path="/editpost" component={EditPost}/>
-              <Route path="/newpost" component={NewPost}/>
-              <Route path="/playground" component={Playground}/>
-              <Route path="/postlist" component={PostList}/>
-              <Route path="/artpostlist" component={ArtPostList}/>
-              <Route path="/tag" component={TagRouter}/>
-            </Switch>
-          </main>
-          <footer>
-            <p>Copyright &copy; 2019 Meguna</p>
-          </footer>
-        </div>
+      <div className="app-container">
+        <header className="app-header">
+          <NavLink exact to="/" activeClassName="title-active">
+            <h1 className="header-title">Meguna&apos;s Playground</h1>
+          </NavLink>
+          <ul className="nav-ul">
+            <li className="nav-li"><NavLink exact to="/">Home</NavLink></li>
+            <li className="nav-li"><NavLink to="/about">About</NavLink></li>
+          </ul>
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/about" component={About} />
+            <Route exact path="/" component={Home} />
+            <Route path="/post/:id" component={Post} />
+          </Switch>
+        </main>
+        <footer>
+          <p>Copyright &copy; 2019 Meguna</p>
+        </footer>
+      </div>
     );
   }
 }
